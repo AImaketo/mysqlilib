43488b6724b1324e08bdb6c0cfc2535fadc92944
AIparent <wade38250@126.com>
2018-02-12 14:58:40 +0800

    mysqli 连接测试

--- mysqli_query.php
+++ mysqli_query.php
@@ -1,3 +1,22 @@
 <?php
-$mysqli = mysqli('localhost','root','','point');
-print_r($mysql);
+//建立到数据库的连接
+//$mysqli = new mysqli('localhost','root','','point');
+//print_r($mysqli);
+$mysqli = new mysqli();
+@$mysqli->connect('127.0.0.1','root','','point');
+//print_r($mysqli);
+//捕获错误信息
+if ($mysqli->connect_errno){
+    die($mysqli->connect_error);
+}
+//设置字符集
+$mysqli->set_charset('utf8');
+//print_r($mysqli->get_connection_stats());
+print_r($mysqli);
+echo '<br/>';
+echo '客户端信息'."\t:".$mysqli->client_info.'<br/>';
+echo "客户端版本:\t".$mysqli->client_version.'<br/>';
+echo '主机信息'.$mysqli->host_info.'<br/>';
+echo '服务器信息'.$mysqli->server_info.'<br/>';
+echo $mysqli->server_version.'<br/>';
+echo $mysqli->get_server_info();
